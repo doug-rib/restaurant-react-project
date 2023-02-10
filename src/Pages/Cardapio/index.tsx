c54844af6d05555836b25edf50c10547267b6b13
Douglas Moreira Ribeiro Junior <ribeiro.douglas5052@gmail.com>
2023-02-10 20:13:14 -0300

    Orderer

--- src/Pages/Cardapio/index.tsx
+++ src/Pages/Cardapio/index.tsx
@@ -2,10 +2,13 @@ import styles from "./Cardapio.module.scss"
 import { ReactComponent as Logo } from "assets/logo.svg"
 import { useState } from "react"
 import Buscador from "./Buscador"
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
 
     const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null)
     
     return (
         <main>
@@ -23,6 +26,13 @@ export default function Cardapio() {
                     busca={busca}
                     setBusca={setBusca}
                 />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros 
+                        filtro={filtro}
+                        setFiltro={setFiltro}
+                    />
+                    <Ordenador />
+                </div>
             </section>
         </main>
     )
