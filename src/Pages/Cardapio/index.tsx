ddbe83b4551805ec43253fd91e04c570835c649f
Douglas Moreira Ribeiro Junior <ribeiro.douglas5052@gmail.com>
2023-02-12 11:07:35 -0300

    update orderer

--- src/Pages/Cardapio/index.tsx
+++ src/Pages/Cardapio/index.tsx
@@ -9,6 +9,7 @@ export default function Cardapio() {
 
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null)
+    const [ordenador, setOrdenador] = useState("")
     
     return (
         <main>
@@ -31,7 +32,7 @@ export default function Cardapio() {
                         filtro={filtro}
                         setFiltro={setFiltro}
                     />
-                    <Ordenador />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
                 </div>
             </section>
         </main>
