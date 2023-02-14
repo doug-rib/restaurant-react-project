35b626b87e85a771cbf7ebed9d007cbb74b142f5
Douglas Moreira Ribeiro Junior <ribeiro.douglas5052@gmail.com>
2023-02-14 20:52:42 -0300

    home page

--- src/Pages/Cardapio/Filtros/index.tsx
+++ src/Pages/Cardapio/Filtros/index.tsx
@@ -1,6 +1,6 @@
-import filtros from "./filtros.json"
-import styles from "./Filtros.module.scss"
-import React from "react"
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+import React from 'react';
 
 type IOpcao = typeof filtros[0]
 
@@ -11,23 +11,23 @@ interface Props {
 
 export default function Filtros({ filtro, setFiltro }: Props) {
 
-    function selecionarFiltro(opcao: IOpcao) {
-        if (filtro === opcao.id) return setFiltro(null)
-        return setFiltro(opcao.id)
-    }
+  function selecionarFiltro(opcao: IOpcao) {
+    if (filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id);
+  }
 
-    return(
-        <div className={styles.filtros}>
-            {filtros.map(opcao => (
-                <button 
-                    className={`
-                    ${styles.filtros__filtro} ${filtro === opcao.id ? styles["filtros__filtro--ativo"] : ""}`} 
-                    key={opcao.id} 
-                    onClick={() => selecionarFiltro(opcao)}
-                >
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    )
+  return(
+    <div className={styles.filtros}>
+      {filtros.map(opcao => (
+        <button 
+          className={`
+                    ${styles.filtros__filtro} ${filtro === opcao.id ? styles['filtros__filtro--ativo'] : ''}`} 
+          key={opcao.id} 
+          onClick={() => selecionarFiltro(opcao)}
+        >
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/Pages/Cardapio/Ordenador/index.tsx
+++ src/Pages/Cardapio/Ordenador/index.tsx
@@ -1,7 +1,7 @@
-import styles from "./Ordenador.module.scss"
-import opcoes from "./opcoes.json"
-import React, { useState } from "react"
-import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md"
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import React, { useState } from 'react';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
 
 interface Props {
     ordenador: string,
@@ -9,19 +9,19 @@ interface Props {
 }
 
 export default function Ordenador({ ordenador, setOrdenador }: Props) {
-    const [aberto, setAberto] = useState(false);
-    const nomeOrdenador = opcoes.find(opcao => opcao.value === ordenador)?.nome
-    return (
-        <button className={`${styles.ordenador} ${ordenador !== "" && styles["ordenador--ativo"]}`} onClick={() => setAberto(!aberto)} onBlur={() => setAberto(false)}>
-            <span>{nomeOrdenador || "Ordenar por"}</span>
-            {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
-            <div className={`${styles.ordenador__options} ${aberto && styles["ordenador__options--ativo"]}`}>
-                {opcoes.map(opcao => (
-                    <div className={styles.ordenador__option} key={opcao.value}  onClick={() => setOrdenador(opcao.value)}>
-                        {opcao.nome}
-                    </div>
-                ))}
-            </div>
-        </button>
-    )
+  const [aberto, setAberto] = useState(false);
+  const nomeOrdenador = opcoes.find(opcao => opcao.value === ordenador)?.nome;
+  return (
+    <button className={`${styles.ordenador} ${ordenador !== '' && styles['ordenador--ativo']}`} onClick={() => setAberto(!aberto)} onBlur={() => setAberto(false)}>
+      <span>{nomeOrdenador || 'Ordenar por'}</span>
+      {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
+      <div className={`${styles.ordenador__options} ${aberto && styles['ordenador__options--ativo']}`}>
+        {opcoes.map(opcao => (
+          <div className={styles.ordenador__option} key={opcao.value}  onClick={() => setOrdenador(opcao.value)}>
+            {opcao.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  );
 }
